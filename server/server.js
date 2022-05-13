b7e6b88c564f440d7755aaa81afba78f86a08b71
ferlobo1985 <fernando.lobo.prez@gmail.com>
2022-05-13 17:38:11 -0300

    03-Starting the server

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const xss = require('xss-clean');
+const mongoSanitize = require('express-mongo-sanitize');
+
+const mongoUri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}?retryWrites=true&w=majority`
+mongoose.connect(mongoUri);
+
+// PARSING
+app.use(bodyParser.json())
+
+
+// SANITIZE
+app.use(xss());
+app.use(mongoSanitize());
+
+
+
+const port = process.env.PORT || 3001;
+app.listen(port,()=>{
+    console.log(`Server running on port ${port}`)
+});
