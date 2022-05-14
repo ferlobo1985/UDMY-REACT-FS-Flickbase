e299ee6aed280716f3a1698bf464cab28ff252eb
ferlobo1985 <fernando.lobo.prez@gmail.com>
2022-05-14 06:26:03 -0300

    15-Update user email

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,7 +1,7 @@
 const httpStatus = require('http-status');
 const { ApiError } = require('../middleware/apiError')
 
-const { userService } = require('../services');
+const { userService, authService } = require('../services');
 
 const userController = {
     async profile(req,res,next){
@@ -19,6 +19,23 @@ const userController = {
         try{
             const user = await userService.updateUserProfile(req)
             res.json(res.locals.permission.filter(user._doc))
+        } catch(error){
+            next(error)
+        }
+    },
+    async updateUserEmail(req,res,next){
+        try{
+            const user = await userService.updateUserEmail(req);
+            const token = await authService.genAuthToken(user);
+            
+            //////
+            console.log('send email to verify');
+
+            res.cookie('x-access-token',token)
+            .send({
+                user: res.locals.permission.filter(user._doc),
+                token
+            })
         } catch(error){
             next(error)
         }
