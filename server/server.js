c2d03e01c70aaff7f305f14742ee5628d62b0fcf
ferlobo1985 <fernando.lobo.prez@gmail.com>
2022-05-14 03:16:54 -0300

    10-Passport jwt

--- server/server.js
+++ server/server.js
@@ -10,6 +10,9 @@ const mongoSanitize = require('express-mongo-sanitize');
 
 const routes = require('./routes');
 
+
+const passport = require('passport');
+const { jwtStrategy } = require('./middleware/passport');
 const { handleError, convertToApiError } = require('./middleware/apiError');
 
 const mongoUri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}?retryWrites=true&w=majority`
@@ -22,6 +25,11 @@ app.use(bodyParser.json())
 // SANITIZE
 app.use(xss());
 app.use(mongoSanitize());
+
+
+// PASSPORT
+app.use(passport.initialize());
+passport.use('jwt',jwtStrategy);
 
 /// routes
 app.use('/api',routes)
