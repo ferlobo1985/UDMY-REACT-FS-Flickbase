5f2de61a1c87b5a38e70b77e7ce817e81e2969eb
ferlobo1985 <fernando.lobo.prez@gmail.com>
2022-05-15 16:11:39 -0300

    41-Add article form 1

--- client/src/components/dashboard/articles/edit_add/add.js
+++ client/src/components/dashboard/articles/edit_add/add.js
@@ -32,6 +32,8 @@ const AddArticle = () => {
     const articles = useSelector(state=>state.articles);
     const dispatch = useDispatch();
 
+    const actorsValue = useRef('');
+
     const formik = useFormik({
         enableReinitialize:true,
         initialValues: formValues,
@@ -46,7 +48,143 @@ const AddArticle = () => {
     return(
         <>
             <AdminTitle title="Add article"/>
-            add article
+           
+            <form className='mt-3 article_form' onSubmit={formik.handleSubmit}>
+
+                <div className='form-group'>
+                    <TextField
+                        style={{ width:'100%'}}
+                        name="title"
+                        label="Enter a title"
+                        variant="outlined"
+                        {...formik.getFieldProps('title')}
+                        {...errorHelper(formik,'title')}
+                    />
+                </div>
+
+                <div className='form-group'>
+                    WYSIWYG
+                </div>
+
+                <div className='form-group'>
+                    <TextField
+                        style={{ width:'100%'}}
+                        name="excerpt"
+                        label="Enter a short desc"
+                        variant="outlined"
+                        {...formik.getFieldProps('excerpt')}
+                        {...errorHelper(formik,'excerpt')}
+                        multiline
+                        rows={4}
+                    />
+                </div>
+
+                <Divider className='mt-3 mb-3'/>
+
+                <div className='form-group'>
+                    <TextField
+                        style={{ width:'100%'}}
+                        name="score"
+                        label="Enter a score"
+                        variant="outlined"
+                        {...formik.getFieldProps('score')}
+                        {...errorHelper(formik,'score')}
+                    />
+                </div>
+
+                <div className='form-group'>
+                    <FormikProvider value={formik}>
+                        <FieldArray
+                            name="actors"
+                            render={ arrayHelpers =>(
+                                <div>
+                                    <Paper className='actors_form'>
+                                        <InputBase
+                                            inputRef={actorsValue}
+                                            className="input"
+                                            placeholder='Add actor name here'
+                                        />
+                                        <IconButton
+                                            onClick={()=>{
+                                                if(actorsValue.current.value !== ''){
+                                                    arrayHelpers.push(actorsValue.current.value)
+                                                }
+                                                actorsValue.current.value = '';
+                                            }}
+                                        >
+                                            <AddIcon/>
+                                        </IconButton>
+                                    </Paper>
+                                    { formik.errors.actors && formik.touched.actors ?
+                                        <FormHelperText error="true">
+                                            { formik.errors.actors}
+                                        </FormHelperText>
+                                    :null}
+
+                                    <div className='chip_container'>
+                                        { formik.values.actors.map((actor,index)=>(
+                                            <div key={index}>
+                                                <Chip
+                                                    label={`${actor}`}
+                                                    color="primary"
+                                                    onDelete={()=> arrayHelpers.remove(index)}
+                                                />
+                                            </div>
+                                        ))}
+                                    </div>
+                                </div> 
+                            )}
+                        />
+                    </FormikProvider>
+                </div>
+
+
+                <div className='form-group'>
+                    <TextField
+                        style={{ width:'100%'}}
+                        name="director"
+                        label="Enter a director"
+                        variant="outlined"
+                        {...formik.getFieldProps('director')}
+                        {...errorHelper(formik,'director')}
+                    />
+                </div>
+
+                <Divider className='mt-3 mb-3'/>
+
+                <FormControl fullWidth>
+                    <InputLabel>Select a status</InputLabel>
+                    <Select
+                        name="status"
+                        label="Select a status"
+                        {...formik.getFieldProps('status')}
+                        error={ formik.errors.status && formik.touched.status ? true:false}
+                    >
+                        <MenuItem value=""><em>None</em></MenuItem>
+                        <MenuItem value="draft">Draft</MenuItem>
+                        <MenuItem value="public">Public</MenuItem>
+                    </Select>
+                    { formik.errors.status && formik.touched.status ?
+                        <FormHelperText error="true">
+                            { formik.errors.status}
+                        </FormHelperText>
+                    :null
+                    }
+                </FormControl>
+
+                <Divider className='mt-3 mb-3'/>
+
+                <Button
+                    variant='contained'
+                    color="primary"
+                    type="submit"
+                >
+                    Add article
+                </Button>
+
+
+            </form>
+
         </>
     )
 
