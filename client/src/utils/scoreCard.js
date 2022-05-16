e897aa65a672f788c2c66774dfe1bfa0b2d3cd92
ferlobo1985 <fernando.lobo.prez@gmail.com>
2022-05-16 05:47:44 -0300

    59-Article view 2

--- client/src/utils/scoreCard.js
+++ client/src/utils/scoreCard.js
@@ -13,10 +13,44 @@ import StarIcon from '@mui/icons-material/Star';
 
 const ScoreCard = ({current}) => {
 
-
     return(
         <>
-            scorecard
+        <hr/>
+            <List className='scorecard'>
+                <ListItem>
+                    <ListItemAvatar>
+                        <Avatar><StarIcon/></Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary="Our score" secondary={current.score} className="rating"/>
+                </ListItem>
+                <Divider variant='inset' component="li"/>
+                <ListItem>
+                    <ListItemAvatar>
+                        <Avatar><PersonIcon/></Avatar>
+                    </ListItemAvatar>
+                    <div>
+                        { current.actors.map((item,index)=>(
+                            <Chip
+                                key={`${item+index}`}
+                                item={item}
+                                label={item}
+                                clickable
+                                color="primary"
+                                className='chip'
+                            />
+                        ))}
+                    </div>
+                </ListItem>
+                <Divider variant='inset' component="li"/>
+                <ListItem>
+                    <ListItemAvatar>
+                        <Avatar><MovieIcon/></Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary="Director" secondary={current.director} />
+                </ListItem>
+            </List>
+           
+
         </>
     )
 }
