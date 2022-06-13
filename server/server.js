9e8a00e1051596a7c1cf0f5d743c41a5766761bb
ferlobo1985 <fernando.lobo.prez@gmail.com>
2022-06-13 17:19:46 -0300

    deploy

--- server/server.js
+++ server/server.js
@@ -41,6 +41,16 @@ app.use((err,req,res,next)=>{
     handleError(err,res)
 })
 
+app.use(express.static('client/build'));
+if(process.env.NODE_ENV === 'production'){
+    const path = require('path');
+
+    app.get('/*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'../client','build','index.html'))
+    })
+}
+
+
 const port = process.env.PORT || 3001;
 app.listen(port,()=>{
     console.log(`Server running on port ${port}`)
